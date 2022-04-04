76f8baf60a2fa4d0cec76475594fd626414562ad
Miguel Leon <mleon95@MacBook-Air-de-Miguel.local>
2022-04-04 17:43:38 -0600

    Version Final

---
+++ drawRectangulo.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+ix = -1
+iy = -1
+dibujando = False
+
+def draw_rectangle(event, x, y, flags, params):
+    global ix, iy, dibujando
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        dibujando = True
+        ix, iy = x, y
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if dibujando == True:
+            cv2.rectangle(img, (ix, iy), (x, y), (0, 152, 231), -1)
+    elif event == cv2.EVENT_LBUTTONUP:
+        dibujando = False
+        cv2.rectangle(img, (ix, iy), (x, y), (0, 152, 231), -1)
+
+img = np.zeros((400, 300, 3), np.int8)
+
+cv2.namedWindow(winname='Tarea IA')
+cv2.setMouseCallback('Tarea IA', draw_rectangle)
+
+while True:
+    cv2.imshow('mi dibujo', img)
+
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+
+        cv2.destroyAllWindow()
